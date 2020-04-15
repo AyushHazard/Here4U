e772b770fde3932231fd66a1549df9510284621e
Divyanshu Mathpal <divyanshumathpal@gmail.com>
2020-04-15 19:56:28 +0530

    Client Databse

--- Mentally/settings.py
+++ Mentally/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'client.apps.ClientConfig',
     'counsellor.apps.CounsellorConfig',
     'django.contrib.admin',
@@ -120,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'/uploads/')
