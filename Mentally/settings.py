027e0282337cb95f07b42fa90bd0f6f79b78e982
Ayush Agrawal <ayush.dps174@gmail.com>
2020-04-15 18:02:24 +0530

    fixed static files

--- Mentally/settings.py
+++ Mentally/settings.py
@@ -119,11 +119,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-path = BASE_DIR + "/static/"
+# path = BASE_DIR + "/static/"
 
-STATIC_URL = path
+STATIC_URL = "/static/"
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-#     '/var/www/static/',
-# ]
+STATICFILES_DIRS =(
+    os.path.join(BASE_DIR, "static"),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
