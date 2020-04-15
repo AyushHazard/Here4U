072d5258cb290119ac5ba42b4b161a3cdc656f7a
Ayush Agrawal <ayush.dps174@gmail.com>
2020-04-15 17:35:42 +0530

    made a common static file for all apps

--- Mentally/settings.py
+++ Mentally/settings.py
@@ -119,4 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+path = BASE_DIR + "/static/"
+
+STATIC_URL = path
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+#     '/var/www/static/',
+# ]
