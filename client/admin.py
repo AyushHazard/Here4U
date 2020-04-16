1db5eb3b0117ab717c8c93526feb8843b04682c4
Divyanshu Mathpal <divyanshumathpal@gmail.com>
2020-04-16 13:31:22 +0530

    Description added and liked to client data

--- client/admin.py
+++ client/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Userdata
+from .models import Userdata,Description
 # Register your models here.
 admin.site.register(Userdata)
+admin.site.register(Description)
--- counsellor/models.py
+++ counsellor/models.py
@@ -1,2 +1,10 @@
 from django.db import models
 # Create your models here.
+class Userdata(models.Model):
+    Name=models.CharField(max_length=100)
+    Gender=models.CharField(max_length=32,choices=[(0,'Male'),(1,'Female'),(2,'Other')])
+    Age=models.IntegerField()
+    Email=models.EmailField()
+    State=models.CharField(max_length=50,null=True)
+    City=models.CharField(max_length=50,null=True)
+    Educational_Status=models.CharField(max_length=32,choices=[(0,'10th'),(1,'12th'),(2,'Graduate'),(3,'Post-Graduate'),(4,'Doctorate'),(5,'Post-Doc')])
