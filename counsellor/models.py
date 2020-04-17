c9e3a3c2dc9ff6617541412eaa4e9137fb02cb1a
Ayush Agrawal <ayush.dps174@gmail.com>
2020-04-17 20:33:21 +0530

    minor changes

--- counsellor/models.py
+++ counsellor/models.py
@@ -3,12 +3,14 @@ from django.db import models
 class Counsellordata(models.Model):
     Name=models.CharField(max_length=100)
     Gender=models.IntegerField(max_length=32,choices=[(0,'Male'),(1,'Female'),(2,'Other')])
-    Age=models.IntegerField()
-    # Profile_pic = models.FileField()
+    Age=models.IntegerField(blank = True,null=True)
+    Profile_pic = models.FileField(blank=True,null=True)
     Email=models.EmailField()
-    State=models.CharField(max_length=50,null=True)
-    City=models.CharField(max_length=50,null=True)
-    Educational_Status=models.CharField(max_length=32,choices=[('a','10th'),('b','12th'),('c','Graduate'),('d','Post-Graduate'),('e','Doctorate'),('f','Post-Doc')])
+    State=models.CharField(max_length=50,blank=True,null=True)
+    City=models.CharField(max_length=50,blank=True,null=True)
+    Education=models.CharField(max_length=300,blank = True,null=True)
+    Expertise = models.CharField(max_length=200,blank=True,null=True)
+    Summary = models.CharField(max_length=1000,blank=True,null=True)
 
     def __str__(self):
         return self.Name
